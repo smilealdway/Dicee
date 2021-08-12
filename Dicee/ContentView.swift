4b135383e05fb57e972e90c39d2a8cc21fd8772d
Katie McBratney <kammcb14@gmail.com>
2021-08-12 12:43:53 -0700

    Initial Commit

---
+++ Dicee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee
+//
+//  Created by Katie McBratney on 8/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
